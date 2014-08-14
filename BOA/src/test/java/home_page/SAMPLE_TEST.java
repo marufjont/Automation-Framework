dff9a49a3752ee3fc0ec8a1d0e80bdd7081e06b8
marufjont <marufjont@gmail.com>
2014-08-14 15:21:06 -0400

    CommonAPI updated; BOA_runner updated;

--- BOA/src/test/java/home_page/SAMPLE_TEST.java
+++ BOA/src/test/java/home_page/SAMPLE_TEST.java
@@ -1,13 +1,25 @@
 package home_page;
 
+import common.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 /**
  * Created by Maruf on 8/11/2014.
  */
-public class SAMPLE_TEST {
+public class SAMPLE_TEST extends CommonAPI{
+
     @Test
     public void mainTest() {
-        System.out.println("Inside main test");
+
+
+        driver.findElement(By.cssSelector("sds")).sendKeys("ssdsd");
+
+
+
     }
+
+
 }
--- Generic/src/main/java/common/CommonAPI.java
+++ Generic/src/main/java/common/CommonAPI.java
@@ -1,7 +1,106 @@
 package common;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Maruf on 8/11/2014.
  */
+
 public class CommonAPI {
+    public static WebDriver driver;
+    @Parameters({"url", "browser"})
+    @BeforeMethod
+    public void setUp(String url, String browser) {
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("IE") || browser.equalsIgnoreCase("Internet Explorer")) {
+            driver = new InternetExplorerDriver();
+        } else if (browser.equalsIgnoreCase("HTMLUnitDriver")) {
+            driver = new HtmlUnitDriver();
+        } else driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.navigate().to(url);
+    }
+
+//    @AfterMethod
+//    public void tearDown() {
+//        driver.quit();
+//    }
+
+public enum SelectorType {
+    CSS, CLASSNAME, XPATH, TAGNAME, ID, LINKTEXT, PARTIALLINKTEXT, NAME
+}
+    static public WebElement getElement(String selectorValue, SelectorType selectorType) {
+        WebElement element=null;
+        switch (selectorType) {
+            case XPATH:
+                element= driver.findElement(By.xpath(selectorValue));
+                break;
+            case CSS:
+                System.out.println("by css");
+                element=driver.findElement(By.cssSelector(selectorValue));
+                break;
+            case CLASSNAME:
+                element= driver.findElement(By.className(selectorValue));
+                break;
+            case TAGNAME:
+                element= driver.findElement(By.tagName(selectorValue));
+                break;
+            case ID:
+                element= driver.findElement(By.id(selectorValue));
+                break;
+            case LINKTEXT:
+                element= driver.findElement(By.linkText(selectorValue));
+                break;
+            case PARTIALLINKTEXT:
+                element= driver.findElement(By.partialLinkText(selectorValue));
+                break;
+            case NAME:
+                element= driver.findElement(By.name(selectorValue));
+                break;
+        }
+        return element;
+    }
+    public static List<WebElement> getElementsList(String selectorValue, SelectorType selectorType) {
+        List<WebElement>elementList=null;
+        switch (selectorType) {
+            case XPATH:
+                elementList= driver.findElements(By.xpath(selectorValue));
+                break;
+            case CSS:
+                elementList=driver.findElements(By.cssSelector(selectorValue));
+                break;
+            case CLASSNAME:
+                elementList= driver.findElements(By.className(selectorValue));
+                break;
+            case TAGNAME:
+                elementList= driver.findElements(By.tagName(selectorValue));
+                break;
+            case ID:
+                elementList= driver.findElements(By.id(selectorValue));
+                break;
+            case LINKTEXT:
+                elementList= driver.findElements(By.linkText(selectorValue));
+                break;
+            case PARTIALLINKTEXT:
+                elementList= driver.findElements(By.partialLinkText(selectorValue));
+                break;
+            case NAME:
+                elementList= driver.findElements(By.name(selectorValue));
+                break;
+        }
+        return elementList;
+    }
+
 }
