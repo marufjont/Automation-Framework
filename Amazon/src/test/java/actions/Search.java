29bc49a4679f334c5e0367afca9af994875211ab
marufjont <marufjont@gmail.com>
2014-08-15 21:38:22 -0400

    Amazon modular and integration test cases added

---
+++ Amazon/src/test/java/actions/Search.java
@@ -0,0 +1,16 @@
+package actions;
+
+import common.CommonAPI;
+import page_factory.SearchObjects;
+
+/**
+ * Created by Maruf on 8/15/2014.
+ */
+public class Search extends CommonAPI{
+    public static void search(String searchTerm)  {
+
+        SearchObjects searchObjects = new SearchObjects(driver);
+        searchObjects.searchField.sendKeys(searchTerm);
+        searchObjects.searchField.submit();
+    }
+}
