ed7d30d2cde0dae6141e1203f2e1873271932a88
Marufjon Temirov <marufjont@gmail.com>
2016-05-16 21:39:13 -0400

    fixed the format

--- Amazon/src/test/java/actions/Search.java
+++ Amazon/src/test/java/actions/Search.java
@@ -6,11 +6,11 @@ import page_factory.SearchObjects;
 /**
  * Created by Maruf on 8/15/2014.
  */
-public class Search extends CommonAPI{
-    public static void search(String searchTerm)  {
+public class Search extends CommonAPI {
+	public static void search(String searchTerm) {
 
-        SearchObjects searchObjects = new SearchObjects(driver);
-        searchObjects.searchField.sendKeys(searchTerm);
-        searchObjects.searchField.submit();
-    }
+		SearchObjects searchObjects = new SearchObjects(driver);
+		searchObjects.searchField.sendKeys(searchTerm);
+		searchObjects.searchField.submit();
+	}
 }
